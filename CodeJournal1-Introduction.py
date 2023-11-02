98b4312123da03fa21495b9ead7658fd62ea8172
gitucsc <146916522+gitucsc@users.noreply.github.com>
2023-11-02 14:13:07 -0700

    Code Journal 1 Introduction

---
+++ CodeJournal1-Introduction.py
@@ -0,0 +1,25 @@
+#This•program•will•introduce
+#Sofia•Rodriguez-Callol!
+
+#This function tells the
+#computer to print 
+#"Hello my name is Sofia" 
+def PrintHellomynameisSofia():
+	print("Hello my name is Sofia.")
+def PrintMyfavoritefoodsincludepickles():
+	print("My favorite foods include pickles.")
+def PrintOneofmyfavoritemoviesistrainspotting():
+	print("One of my favorite movies is trainspotting.")
+
+#This defines our main()
+#the function of our program
+def main():
+	PrintHellomynameisSofia()
+	PrintMyfavoritefoodsincludepickles()
+	PrintOneofmyfavoritemoviesistrainspotting()
+
+#when we run the program
+#this excecutes first
+if __name__ == '__main__':
+	main()
+
