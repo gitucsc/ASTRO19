42185658472eb0aa2f975f11fa8e32844c793935
gitucsc <146916522+gitucsc@users.noreply.github.com>
2023-11-02 21:36:45 -0700

    Code Journal 1 pt3 uploaded

---
+++ CodeJournal1-pt3.py
@@ -0,0 +1,16 @@
+#This program will define
+#a function f(x) that returns
+#a result of x**3 + 8
+
+def f(x):
+	return x**3+8
+
+#Define x and print
+x=input("9")
+x=float(9)
+print("f(x) equals",f(x))
+
+#resulted in a number 
+#higher than 27! 
+
+print("YAY!")
